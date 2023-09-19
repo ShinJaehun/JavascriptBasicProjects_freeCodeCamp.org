88e122aecd327c10745360390ac22a902ac85aa5
ShinJaehun <gunsmoke.shin@gmail.com>
2023-09-19 11:41:56 +0900

    finish pagination

--- 27-pagination/setup/displayButtons.js
+++ 27-pagination/setup/displayButtons.js
@@ -1,3 +1,17 @@
-const displayButtons = () => {}
+const displayButtons = (container, pages, activeIndex) => {
+    // console.log(pages)
+    let btns = pages.map((_, pageIndex) => {
+        // console.log(a) // 0~9까지 array
+        // console.log(pageIndex)
+        return `<button class="page-btn ${activeIndex === pageIndex
+                    ? 'active-btn'
+                    : 'null' }" data-index="${pageIndex}">
+                    ${pageIndex + 1}
+                </button>`
+    })
+    btns.push(`<button class="next-btn">next</button>`)
+    btns.unshift(`<button class="prev-btn">prev</button>`)
+    container.innerHTML = btns.join('')
+}
 
 export default displayButtons
--- 27-pagination/setup/paginate.js
+++ 27-pagination/setup/paginate.js
@@ -1,3 +1,15 @@
-const paginate = () => {}
+const paginate = (followers) => {
+    const itemsPerPage = 10
+    const numberOfPages = Math.ceil(followers.length / itemsPerPage)
+
+    const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+        // 의도하지 않았지만 loop를 돌게 됨
+        // index가 numberOfPages에 가까워질때까지
+        // console.log(index)
+        const start = index * itemsPerPage
+        return followers.slice(start, start + itemsPerPage) 
+    }) 
+    return newFollowers
+}
 
 export default paginate
